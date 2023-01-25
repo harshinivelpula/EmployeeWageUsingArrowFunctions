1cfc5715bde976f8040d2f17b28ad1a259735e22
DELL <105187636+harshinivelpula@users.noreply.github.com>
2023-01-25 20:23:57 +0530

    [charu]create new class and extend payrolldata to store gender,startdate

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -325,3 +325,35 @@ let nonWorkingDayNum=empDailyHrsAndWageArr
         .filter(dailyHrsAndWage=>dailyHrsAndWage.dailyHours==0)
         .map(dailyHrsAndWage=>dailyHrsAndWage.dayNum);
 console.log("\nUC 11D NonWorkingDayNums: "+nonWorkingDayNum);
+//uc12
+class EmployeePayrollData
+{
+    id;
+    salary;
+    gender;
+    startDate;
+
+    constructor(id,name,salary)
+    {
+        this.id=id;
+        this.name=name;
+        this.salary=salary;
+    }
+
+    get name(){return this._name;}
+    set name(name)
+    {
+        console.log("Setting: "+name);
+        this._name=name;
+    }
+    toString()
+    {
+        return "id=" +this.id+", name="+this.name+", salary="+this.salary;
+    }
+}
+
+let employeePayrollData=new EmployeePayrollData(1, "charu", 30000);
+console.log(employeePayrollData.toString());
+employeePayrollData.id=0;
+employeePayrollData.name="chocky";
+console.log(employeePayrollData.toString());

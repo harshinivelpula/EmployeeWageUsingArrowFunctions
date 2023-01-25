c9826c54ac29f15fe8f472776bea92a4c4811a83
DELL <105187636+harshinivelpula@users.noreply.github.com>
2023-01-25 19:41:49 +0530

    [charu]cal wage using filter function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -198,27 +198,35 @@ const MAX_HRS_INMONTH=160;
     }
     let empWage = calcDailyWage(totalEmpHrs);
     console.log("UC6 - Total Days: " + totalWorkingDays + "\nTotal Hours: " + totalEmpHrs +"\nEmployee Wage: " + empWage);
+//UC 7A
+let totEmpWage=0;
+function sum(dailyWage)
+{
+    totEmpWage += dailyWage;
+}
+empDailyWageArr.forEach(sum);
+console.log("\nUC7A - Total Days: " + totalWorkingDays + "\nTotal Hours: " + totalEmpHrs +"\nEmployee Wage: " + totEmpWage);
 
-    let totEmpWage=0;
-    function sum(dailyWage)
-    {
-        totEmpWage += dailyWage;
-    }
-    empDailyWageArr.forEach(sum);
-    console.log("\nUC7A - Total Days: " + totalWorkingDays + "\nTotal Hours: " + totalEmpHrs +"\nEmployee Wage: " + totEmpWage);
+function totalWages(totalWage,dailyWage){
+    return totalWage + dailyWage;
+}
+console.log("UC7A - Emp Wage With Reduce: "+empDailyWageArr.reduce(totalWages,0));
 
-    function totalWages(totalWage,dailyWage){
-        return totalWage + dailyWage;
-    }
-    console.log("UC7A - Emp Wage With Reduce: "+empDailyWageArr.reduce(totalWages,0));
+//UC 7B
+let dailyCntr = 0;
+function mapDayWithWage(dailyWage){
+    dailyCntr++;
+    return dailyCntr + " = " + dailyWage;
+}
+let mapDayWithWageArr = empDailyWageArr.map(mapDayWithWage);
+console.log("\nUC7B - Daily Wage Map");
+console.log(mapDayWithWageArr);
 
-    let dailyCntr = 0;
-    function mapDayWithWage(dailyWage){
-        dailyCntr++;
-        return dailyCntr + " = " + dailyWage;
-    }
-    let mapDayWithWageArr = empDailyWageArr.map(mapDayWithWage);
-    console.log("\n UC7B - Daily Wage Map");
-    console.log(mapDayWithWageArr);
-
+//UC 7C-Show Days When Full Time Wage of 160 were earned
+function fulltimeWage(dailyWage) {
+    return dailyWage.includes("160");
+}
+let fullDayWageArr = mapDayWithWageArr.filter(fulltimeWage);
+console.log("\nUC7C - Daily Wage Filter When Fulltime Wage Earned");
+console.log(fullDayWageArr);
 }

8c938655b32fefc0fcc3dc40299043992b5da1d4
DELL <105187636+harshinivelpula@users.noreply.github.com>
2023-01-25 19:00:59 +0530

    [charu]check if emp is present or absent

---
+++ EmployeeWage.js
@@ -0,0 +1,13 @@
+//UC1 IF ELSE CONDITION
+{
+    const IS_ABSENT=0
+    let empCheck = Math.floor(Math.random() * 10) % 2;
+    if(empCheck==IS_ABSENT)
+    {
+        console.log("EMPLOYEE IS ABSENT");
+    }
+    else
+    {
+        console.log("EMPLOYEE IS PRESENT");
+    }
+}
